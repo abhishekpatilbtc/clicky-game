fca25465d009588fa5d009e58ed5eb8eed1c50bc
Abhishek Patil <abhishekpatil@vmware.com>
2020-01-29 12:36:36 -0500

    css changes

--- src/components/js/header.js
+++ src/components/js/header.js
@@ -5,7 +5,6 @@ class Header extends Component{
     render(){
         return (
            <header className="jumbotron">
-               <h1 className="display-4">Clicky Game!</h1>
                <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
            </header>
         )
